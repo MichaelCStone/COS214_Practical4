35112061b7cc534dbc95722f5f5a48ff2ade3b4c
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2024-09-08 21:35:35 +0200

    FarmUnit pure virtual

--- FarmUnit.h
+++ FarmUnit.h
@@ -9,13 +9,13 @@
 class FarmUnit
 {
     public:
-        virtual void increaseProduction();
-        virtual void harvestCrops(int currAmount);
-        virtual int getLeftOverCapacity();
-        virtual int getTotalCapacity();
-        virtual void harvestCrops(int currentAmountOfCrops);
-        virtual void setState(SoilState* state);
-        virtual std::string getSoilStateName();
+        virtual void increaseProduction() = 0;
+        virtual void harvestCrops(int currAmount) = 0;
+        virtual int getLeftOverCapacity() = 0;
+        virtual int getTotalCapacity() = 0;
+        virtual void harvestCrops(int currentAmountOfCrops) = 0;
+        virtual void setState(SoilState* state) = 0;
+        virtual std::string getSoilStateName() = 0;
 
 
 };
